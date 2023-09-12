e2a18f3f1b32a440d073abdd487f35a86f410d4b
ValentineOO <varlomxg@gmail.com>
2023-09-12 17:10:08 +0100

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,28 @@
 import { useEffect } from "react";
-import { BrowserRouter, 
-  Routes, 
-  Route, 
-  useLocation } from "react-router-dom";
-  import Home from "./scenes/home/Home";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
 import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
-  
- const ScrollToTop = () => {
-    const { pathname } = useLocation();
-  
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
-  
-    return null;
-  }; 
+import Navbar from "./scenes/global/Navbar";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => {
   return (
-    <div className='app'>
+    <div className="app">
       <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="item/:itemId" element={<ItemDetails />} />
           <Route path="checkout" element={<Checkout />} />
@@ -31,7 +30,7 @@ const App = () => {
         </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,23 @@ import './index.css'
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./state";
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
       <CssBaseline/>
-    <App />
+      <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
-)
+);
