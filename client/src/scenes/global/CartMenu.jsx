0a9a4ffc716acb7155a8c2de014a8f6716088f68
ValentineOO <varlomxg@gmail.com>
2023-09-14 12:13:13 +0100

    working so far

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -73,18 +73,72 @@ const CartMenu = () => {
                     />
                   </Box>
                   <Box flex="1 1 60%">
+                    {/* ITEM NAME */}
                     <FlexBox mb="5px">
                       <Typography fontWeight="bold">
                         {item.attributes.name}
                       </Typography>
-                      <IconButton onClick={() => dispatch(removeFromCart({id: item.id}))}>
-                        <CloseIcon/>
+                      <IconButton
+                        onClick={() =>
+                          dispatch(removeFromCart({ id: item.id }))
+                        }
+                      >
+                        <CloseIcon />
                       </IconButton>
                     </FlexBox>
+                    <Typography>{item.attributes.shortDescription}</Typography>
+
+                    {/* AMOUNT */}
+                    <FlexBox m="15px 0">
+                      <Box border={`1.5px solid ${shades.neutral[500]}`}>
+                        <IconButton
+                          onClick={() =>
+                            dispatch(decreaseCount({ id: item.id }))
+                          }
+                        >
+                          <RemoveIcon />
+                        </IconButton>
+                        <Typography>{item.count}</Typography>
+                        <IconButton
+                          onClick={() =>
+                            dispatch(increaseCount({ id: item.id }))
+                          }
+                        >
+                          <addIcon />
+                        </IconButton>
+                      </Box>
+                    </FlexBox>
                   </Box>
+                  {/* PRICE */}
+                  <Typography>${item.attributes.price}</Typography>
                 </FlexBox>
+                <Divider />
               </Box>
             ))}
+          </Box>
+
+          {/* ACTIONS */}
+          <Box m="20px 0">
+            <FlexBox m="20px 0">
+              <Typography fontWeight="bold">SUBTOTAL</Typography>
+              <Typography fontWeight="bold">${totalPrice}</Typography>
+            </FlexBox>
+            <Button
+              sx={{
+                backgroundColor: shades.primary[400],
+                color: "white",
+                borderRadius: 0,
+                minWidth: "100%",
+                padding: "20px 40px",
+                m: "20px 0",
+              }}
+              onClick={() => {
+                navigate("/checkout");
+                dispatch(setIsCartOpen({}));
+              }}
+            >
+              CHECKOUT
+            </Button>
           </Box>
         </Box>
       </Box>
