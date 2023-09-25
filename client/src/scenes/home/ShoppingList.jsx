14f629f3d931c90595fb21051d618abdc3af2121
ValentineOO <varlomxg@gmail.com>
2023-09-25 16:57:19 +0100

    fixed newArrivals bug

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -74,7 +74,7 @@ const ShoppingList = () => {
           items.map((item) => (
             <Item item={item} key={`${item.name}-${item.id}`} />
           ))}
-        {value === "newArrival" &&
+        {value === "newArrivals" &&
           newArrivalsItems.map((item) => (
             <Item item={item} key={`${item.name}-${item.id}`} />
           ))}
