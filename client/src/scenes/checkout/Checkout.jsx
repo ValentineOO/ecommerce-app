cde9efd6369f1b01be188d6c38c1bb9653f6ba9f
ValentineOO <varlomxg@gmail.com>
2023-09-25 16:38:57 +0100

    done

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -4,8 +4,13 @@ import { Formik } from "formik";
 import { useState } from "react";
 import * as yup from "yup";
 import Shipping from "./Shipping";
-import Payment from  "./Payment";
+import Payment from "./Payment";
 import { shades } from "../../theme";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51NuB27JhmqiSa463CSwAmXe3VXxAUGNjIWPlBsXWHLWduLbAxM1DA2bUQnJSZXRJ2takfnyoJRQRHFWgjgiIc39j00ythNLpAI"
+);
 
 const initialValues = {
   billingAddress: {
@@ -109,7 +114,27 @@ const Checkout = () => {
     actions.setTouched({});
   };
 
-  async function makePayment(values) {}
+  async function makePayment(values) {
+    const stripe = await stripePromise;
+    const requestBody = {
+      userName: [values.firstName, values.lastName].join(" "),
+      email: values.email,
+      products: cart.map(({ id, count }) => ({
+        id,
+        count,
+      })),
+    };
+
+    const response = await fetch("http://localhost:1337/api/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
+    });
+    const session = await response.json();
+    await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+  }
   return (
     <Box width="80%" m="100px auto">
       <Stepper activeStep={activeStep} sx={{ m: "20px 0" }}>
@@ -186,7 +211,6 @@ const Checkout = () => {
                     borderRadius: 0,
                     padding: "15px 40px",
                   }}
-                  onClick={() => setActiveStep(activeStep - 1)}
                 >
                   {isFirstStep ? "Next" : "Place Order"}
                 </Button>
--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,10 +1,15 @@
+import { Box, Alert, AlertTitle } from "@mui/material";
 
 const Confirmation = () => {
   return (
-    <div>
-      Confirmation
-    </div>
-  )
-}
+    <Box m="90px auto" width="80%" height="50vh">
+      <Alert severity="success">
+        <AlertTitle> Success </AlertTitle>
+        You have successfully made an Order -{" "}
+        <strong>Congrats on making your purchase</strong>
+      </Alert>
+    </Box>
+  );
+};
 
-export default Confirmation
+export default Confirmation;
